8b873c05954bdb74102bb11d226faf45b397ecc7
popcorn150 <hungrymanchannel@gmail.com>
2024-04-16 23:27:14 +0100

    scratching the surface

---
+++ index.js
@@ -0,0 +1,5 @@
+const Person = require('./person_class')
+
+const person1 = new Person('Dave', 32)
+
+person1.greetings()
---
+++ person_object.js
@@ -0,0 +1,16 @@
+const person = {
+    name: 'David',
+    age: 27,
+    gender: 'Male',
+    isSingle: true,
+    occupation: 'Coder Coder',
+    favoriteColor: 'Black',
+    isTall: true,
+    likesFood: true,
+    placesTravelled: ['Brazil', 'France', 'Iceland', 'Jamaica'],
+    lovesCoding: true,
+    lovesNoisyAreas: false,
+    hoursUsedToCode: 20
+}
+
+module.exports = person
