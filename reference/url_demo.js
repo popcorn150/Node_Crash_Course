be71c7b8a138174db71650966cf9bb3c66bed58e
popcorn150 <hungrymanchannel@gmail.com>
2024-04-19 23:30:28 +0100

    changes

--- reference/url_demo.js
+++ reference/url_demo.js
@@ -1,1 +1,24 @@
 const url = require('url')
+
+const myUrl = new URL(
+    'http://mywebsite.com:8000/hello.html?id=1000&status=active'
+)
+
+/**Serialized URL */
+console.log(myUrl.href)
+console.log(myUrl.toString())
+/**Host (root domain) */
+console.log(myUrl.host)
+/**Hostname (does not get port) */
+console.log(myUrl.hostname)
+/**Pathname */
+console.log(myUrl.pathname)
+/**Serialized query */
+console.log(myUrl.search)
+/**Params object */
+console.log(myUrl.searchParams)
+/**Add params */
+myUrl.searchParams.append('abc', '123')
+console.log(myUrl.searchParams)
+/**Loop through params */
+myUrl.searchParams.forEach((value, name) => console.log(`${name}: ${value}`))
