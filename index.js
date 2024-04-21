10e55e78cec83fdff8d7588edfd64c5e88da8944
popcorn150 <hungrymanchannel@gmail.com>
2024-04-21 22:19:26 +0100

    runs logger

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const Person = require('./person_class')
+const Logger = require('./logger')
 
-const person1 = new Person('Dave', 32)
+const logger = new Logger()
 
-person1.greetings()
+logger.on('message', data => console.log('Called Listener', data))
+
+logger.log('Hey There!')
